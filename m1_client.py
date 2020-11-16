6fd3cbf717b833d7ca0c4d1dff907f67c9cf9aec
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2020-11-16 10:18:40 +0100

    Update m1_client.py

--- m1_client.py
+++ m1_client.py
@@ -8,10 +8,5 @@ while True:
 
     voltage_reg_address = 107
     voltage_reg_value = client.read_holding_registers(voltage_reg_address,3)
-    v = client.read_input_registers(voltage_reg_address, 1)
-    Curr_reg_address = 102
-    Curr_reg_value = client.read_holding_registers(Curr_reg_address)
-    # voltage_actual_value = 0.018 * voltage_reg_value[]
     print("Voltage:\t", voltage_reg_value)
-    print("Voltage:\t", v)
     sleep(3)
